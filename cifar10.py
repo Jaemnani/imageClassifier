dae306def2a19b93a40e3a791c1337533cda68b6
dpwoans@gmail.com <dpwoans@gmail.com>
2024-06-18 19:01:34 +0900

    update datasets to class

--- cifar10.py
+++ cifar10.py
@@ -2,47 +2,98 @@ import numpy as np
 import os
 import pickle
 
-def load_cifar10_batch(filename):
-    with open(filename, 'rb') as f:
-        # pickle 포맷으로 데이터 로드
-        data_dict = pickle.load(f, encoding='bytes')
-        images = data_dict[b'data']  # 이미지 데이터
-        labels = data_dict[b'labels']  # 레이블
+class DATASET_CIFAR10:
+    def __init__(self, cifar10_dir='./datasets/cifar10/cifar-10-batches-py'):
+        self.cifar10_dir = cifar10_dir
         
-        # 이미지 데이터 형식 변경: (num_samples, height, width, channels)
-        images = images.reshape(-1, 3, 32, 32)  # CIFAR-10 이미지는 32x32 크기의 RGB 이미지임
-        # 순서 변경: (num_samples, height, width, channels) -> (num_samples, channels, height, width)
-        images = images.transpose(0, 2, 3, 1)
+        self.train_images = None
+        self.train_labels = None
+        self.test_images = None
+        self.test_labels = None
         
-        return images, labels
+        self.train_images, self.train_labels, self.test_images, self.test_labels = self.load_cifar10_data(self.cifar10_dir)
 
-def load_cifar10_data(data_dir):
-    images_train = []
-    labels_train = []
-    for i in range(1, 6):
-        filename = os.path.join(data_dir, f'data_batch_{i}')
-        images, labels = load_cifar10_batch(filename)
-        images_train.append(images)
-        labels_train.append(labels)
+    def load_cifar10_batch(self, filename):
+        with open(filename, 'rb') as f:
+            # pickle 포맷으로 데이터 로드
+            data_dict = pickle.load(f, encoding='bytes')
+            images = data_dict[b'data']  # 이미지 데이터
+            labels = np.array(data_dict[b'labels'])  # 레이블
+            
+            # 이미지 데이터 형식 변경: (num_samples, height, width, channels)
+            images = images.reshape(-1, 3, 32, 32)  # CIFAR-10 이미지는 32x32 크기의 RGB 이미지임
+            # 순서 변경: (num_samples, height, width, channels) -> (num_samples, channels, height, width)
+            images = images.transpose(0, 2, 3, 1)
+            
+            return images, labels
+
+    def load_cifar10_data(self, data_dir):
+        train_images = []
+        train_labels = []
+        for i in range(1, 6):
+            filename = os.path.join(data_dir, f'data_batch_{i}')
+            images, labels = self.load_cifar10_batch(filename)
+            train_images.append(images)
+            train_labels.append(labels)
+        
+        # 훈련 데이터 배열 합치기
+        train_images = np.concatenate(train_images, axis=0)
+        train_labels = np.concatenate(train_labels, axis=0)
+        
+        # 테스트 데이터 읽기
+        filename = os.path.join(data_dir, 'test_batch')
+        test_images, test_labels = self.load_cifar10_batch(filename)
+        
+        return train_images, train_labels, test_images, test_labels        
+
+tmp = DATASET_CIFAR10()
+print(f'Train images shape: {tmp.train_images.shape}')
+print(f'Train labels shape: {tmp.train_labels.shape}')
+print(f'Test  images shape: {tmp.test_images.shape}')
+print(f'Test  labels shape: {tmp.test_labels.shape}')
+
+
+# def load_cifar10_batch(filename):
+#     with open(filename, 'rb') as f:
+#         # pickle 포맷으로 데이터 로드
+#         data_dict = pickle.load(f, encoding='bytes')
+#         images = data_dict[b'data']  # 이미지 데이터
+#         labels = data_dict[b'labels']  # 레이블
+        
+#         # 이미지 데이터 형식 변경: (num_samples, height, width, channels)
+#         images = images.reshape(-1, 3, 32, 32)  # CIFAR-10 이미지는 32x32 크기의 RGB 이미지임
+#         # 순서 변경: (num_samples, height, width, channels) -> (num_samples, channels, height, width)
+#         images = images.transpose(0, 2, 3, 1)
+        
+#         return images, labels
+
+# def load_cifar10_data(data_dir):
+#     train_images = []
+#     train_labels = []
+#     for i in range(1, 6):
+#         filename = os.path.join(data_dir, f'data_batch_{i}')
+#         images, labels = load_cifar10_batch(filename)
+#         train_images.append(images)
+#         train_labels.append(labels)
     
-    # 훈련 데이터 배열 합치기
-    images_train = np.concatenate(images_train, axis=0)
-    labels_train = np.concatenate(labels_train, axis=0)
+#     # 훈련 데이터 배열 합치기
+#     train_images = np.concatenate(train_images, axis=0)
+#     train_labels = np.concatenate(train_labels, axis=0)
     
-    # 테스트 데이터 읽기
-    filename = os.path.join(data_dir, 'test_batch')
-    images_test, labels_test = load_cifar10_batch(filename)
+#     # 테스트 데이터 읽기
+#     filename = os.path.join(data_dir, 'test_batch')
+#     test_images, test_labels = load_cifar10_batch(filename)
     
-    return images_train, labels_train, images_test, labels_test
+#     return train_images, train_labels, test_images, test_labels
 
-# CIFAR-10 데이터셋이 저장된 디렉토리 경로
-cifar10_dir = './datasets/cifar10/cifar-10-batches-py'  # 실제 디렉토리 경로로 변경해야 함
+# # CIFAR-10 데이터셋이 저장된 디렉토리 경로
+# cifar10_dir = './datasets/cifar10/cifar-10-batches-py'  # 실제 디렉토리 경로로 변경해야 함
 
-# CIFAR-10 데이터셋 로드
-images_train, labels_train, images_test, labels_test = load_cifar10_data(cifar10_dir)
+# # CIFAR-10 데이터셋 로드
+# train_images, train_labels, test_images, test_labels = load_cifar10_data(cifar10_dir)
 
-# 데이터 확인
-print(f'Train images shape: {images_train.shape}')
-print(f'Train labels shape: {len(labels_train)}')
-print(f'Test images shape: {images_test.shape}')
-print(f'Test labels shape: {len(labels_test)}')
+# # 데이터 확인
+# print(f'Train images shape: {train_images.shape}')
+# print(f'Train labels shape: {len(train_labels)}')
+# print(f'Test images shape: {test_images.shape}')
+# print(f'Test labels shape: {len(test_labels)}')
--- mnist.py
+++ mnist.py
@@ -1,47 +1,98 @@
 import numpy as np
 
-def load_mnist_images(filename):
-    with open(filename, 'rb') as f:
-        # Magic number와 이미지 개수를 읽음
-        magic = int.from_bytes(f.read(4), byteorder='big')
-        num_images = int.from_bytes(f.read(4), byteorder='big')
-        num_rows = int.from_bytes(f.read(4), byteorder='big')
-        num_cols = int.from_bytes(f.read(4), byteorder='big')
+class DATASET_MNIST:
+    def __init__(self, mnist_dir="./datasets/mnist/"):
+        self.train_images_file = mnist_dir + 'train-images.idx3-ubyte'
+        self.train_labels_file = mnist_dir + 'train-labels.idx1-ubyte'
+        self.test_images_file  = mnist_dir + 't10k-images.idx3-ubyte'
+        self.test_labels_file  = mnist_dir + 't10k-labels.idx1-ubyte'
         
-        # 이미지 데이터를 읽고 numpy 배열로 변환
-        images = np.frombuffer(f.read(), dtype=np.uint8)
-        images = images.reshape(num_images, num_rows, num_cols)
+        self.train_images = None
+        self.train_labels = None
+        self.test_images = None
+        self.test_labels = None
         
-        return images
+        self.load_mnist()
+    
+    def load_mnist(self):
+        self.train_images = self.load_mnist_images(self.train_images_file)
+        self.train_labels = self.load_mnist_labels(self.train_labels_file)
+        self.test_images  = self.load_mnist_images (self.test_images_file)
+        self.test_labels  = self.load_mnist_labels (self.test_labels_file)    
+    
+    def load_mnist_images(self, filename):
+        with open(filename, 'rb') as f:
+            # Magic number와 이미지 개수를 읽음
+            magic = int.from_bytes(f.read(4), byteorder='big')
+            num_images = int.from_bytes(f.read(4), byteorder='big')
+            num_rows = int.from_bytes(f.read(4), byteorder='big')
+            num_cols = int.from_bytes(f.read(4), byteorder='big')
+            
+            # 이미지 데이터를 읽고 numpy 배열로 변환
+            images = np.frombuffer(f.read(), dtype=np.uint8)
+            images = images.reshape(num_images, num_rows, num_cols)
+            
+            return images
 
-def load_mnist_labels(filename):
-    with open(filename, 'rb') as f:
-        # Magic number와 레이블 개수를 읽음
-        magic = int.from_bytes(f.read(4), byteorder='big')
-        num_items = int.from_bytes(f.read(4), byteorder='big')
+    def load_mnist_labels(self, filename):
+        with open(filename, 'rb') as f:
+            # Magic number와 레이블 개수를 읽음
+            magic = int.from_bytes(f.read(4), byteorder='big')
+            num_items = int.from_bytes(f.read(4), byteorder='big')
+            
+            # 레이블 데이터를 읽고 numpy 배열로 변환
+            labels = np.frombuffer(f.read(), dtype=np.uint8)
+            
+            return labels
         
-        # 레이블 데이터를 읽고 numpy 배열로 변환
-        labels = np.frombuffer(f.read(), dtype=np.uint8)
+tmp = DATASET_MNIST()
+print(f'Train images shape: {tmp.train_images.shape}')
+print(f'Train labels shape: {tmp.train_labels.shape}')
+print(f'Test images shape:  {tmp.test_images.shape}')
+print(f'Test labels shape:  {tmp.test_labels.shape}')
         
-        return labels
+# def load_mnist_images(filename):
+#     with open(filename, 'rb') as f:
+#         # Magic number와 이미지 개수를 읽음
+#         magic = int.from_bytes(f.read(4), byteorder='big')
+#         num_images = int.from_bytes(f.read(4), byteorder='big')
+#         num_rows = int.from_bytes(f.read(4), byteorder='big')
+#         num_cols = int.from_bytes(f.read(4), byteorder='big')
+        
+#         # 이미지 데이터를 읽고 numpy 배열로 변환
+#         images = np.frombuffer(f.read(), dtype=np.uint8)
+#         images = images.reshape(num_images, num_rows, num_cols)
+        
+#         return images
+
+# def load_mnist_labels(filename):
+#     with open(filename, 'rb') as f:
+#         # Magic number와 레이블 개수를 읽음
+#         magic = int.from_bytes(f.read(4), byteorder='big')
+#         num_items = int.from_bytes(f.read(4), byteorder='big')
+        
+#         # 레이블 데이터를 읽고 numpy 배열로 변환
+#         labels = np.frombuffer(f.read(), dtype=np.uint8)
+        
+#         return labels
 
 
-# 데이터 파일 경로
-train_images_file = './datasets/mnist/train-images.idx3-ubyte'
-train_labels_file = './datasets/mnist/train-labels.idx1-ubyte'
-test_images_file = './datasets/mnist/t10k-images.idx3-ubyte'
-test_labels_file = './datasets/mnist/t10k-labels.idx1-ubyte'
+# # 데이터 파일 경로
+# train_images_file = './datasets/mnist/train-images.idx3-ubyte'
+# train_labels_file = './datasets/mnist/train-labels.idx1-ubyte'
+# test_images_file = './datasets/mnist/t10k-images.idx3-ubyte'
+# test_labels_file = './datasets/mnist/t10k-labels.idx1-ubyte'
 
-# MNIST 데이터셋 로드
-train_images = load_mnist_images(train_images_file)
-train_labels = load_mnist_labels(train_labels_file)
-test_images = load_mnist_images(test_images_file)
-test_labels = load_mnist_labels(test_labels_file)
+# # MNIST 데이터셋 로드
+# train_images = load_mnist_images(train_images_file)
+# train_labels = load_mnist_labels(train_labels_file)
+# test_images = load_mnist_images(test_images_file)
+# test_labels = load_mnist_labels(test_labels_file)
 
-# 데이터 확인
-print(f'Train images shape: {train_images.shape}')
-print(f'Train labels shape: {train_labels.shape}')
-print(f'Test images shape: {test_images.shape}')
-print(f'Test labels shape: {test_labels.shape}')
+# # 데이터 확인
+# print(f'Train images shape: {train_images.shape}')
+# print(f'Train labels shape: {train_labels.shape}')
+# print(f'Test images shape: {test_images.shape}')
+# print(f'Test labels shape: {test_labels.shape}')
 
-print("done")
+# print("done")
